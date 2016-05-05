85e7e3ce71681372ffdf6b678fc16d4bf442b849
Dirc Wang <dirk.wang@samesamechina.com>
2016-05-05 15:18:36 +0800

    change 2

--- Lib/DatabaseAPI.php
+++ Lib/DatabaseAPI.php
@@ -18,7 +18,7 @@ class DatabaseAPI {
 	public function insertUser($data){
 		$sql = "INSERT INTO `lounge_user` SET `useid` = ?, `title` = ?, `firstname` = ?, `lastname` = ?, `mobile` = ?, `email` = ?, `city` = ?, `getmsg` = ?";
 		$res = $this->db->prepare($sql);
-		$res->bind_param("sssssssi", $data['useid'], $data['title'], $data['firstname'], $data['lastname'], $data['mobile'], $data['email'], $data['city'], intval($data['getmsg']));
+		$res->bind_param("ssssssss", $data['useid'], $data['title'], $data['firstname'], $data['lastname'], $data['mobile'], $data['email'], $data['city'], $data['getmsg']);
 		if($res->execute())
 			return true;
 		return false;
